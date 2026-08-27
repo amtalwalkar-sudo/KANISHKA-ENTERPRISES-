26973245d49f08b48de922c2583954d7b581d40d
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-08-27 18:04:13 +0530

    refactor: remove archived business repository

--- src/infrastructure/repositories/workRepository.js
+++
@@ -1,7 +0,0 @@
-import {db} from '../db/database.js';
-export const workRepository={
-  async get(id){return db.work.get(id)},
-  async put(record){return db.work.put(record)},
-  async delete(id){return db.work.delete(id)},
-  async list(){return db.work.toArray()}
-};
