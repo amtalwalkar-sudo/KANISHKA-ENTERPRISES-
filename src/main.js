2d9c1cb6132fc49dfa6dbcf7d2beb7a9c0ccfbd3
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-08-27 13:55:18 +0530

    refactor: use functional seven view models at app bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './ui/App.vue';
 import './ui/styles.css';
-import { installRuntimeContract } from './application/runtimeContract.js';
+import { installRuntimeContract } from './application/runtimeContractV2.js';
 import { registerServiceWorker } from './pwa/register.js';
 
 installRuntimeContract();
